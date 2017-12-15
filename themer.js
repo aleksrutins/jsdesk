4411e5c886f310b03ec88da79d5b54b7d64c5e0c
Aleks Rutins <alex@thehat.hattydomain>
2017-12-15 16:02:13 -0500

    add some macos theme changes

--- themer.js
+++ themer.js
@@ -1,13 +1,26 @@
 var save = wnd.querySelector("#save");
 var theme = wnd.querySelector("#theme");
+var ios = wnd.querySelector("#ios");
 if(localStorage["theme"]) {
   for (let i = 0; i < theme.options.length; i++) {
     if(theme.options[i].value == localStorage["theme"]) {
       theme.selectedIndex = i;
     }
   }
+  if(localStorage["theme"] === "macosx") {
+    ios.style.display = "inline";
+    ios.querySelector("#ioscb").checked = localStorage["iOS"] == "true";
+  }
+}
+theme.onchange = (e) => {
+  if(theme.selectedOptions[0].value == "macosx") {
+    ios.style.display = "inline";
+  } else {
+    ios.style.display = "none";
+  }
 }
 save.onclick = function() {
   localStorage["theme"] = theme.selectedOptions[0].value;
+  localStorage["iOS"] = ios.querySelector("#ioscb").checked;
   location.reload();
 }
